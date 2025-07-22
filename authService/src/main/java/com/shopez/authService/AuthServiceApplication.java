2dc9659c2e13ab6011400e5c95d67dd975b1532b
shubhamsingh <singhshubhamkumar66@gmail.com>
2025-07-22 22:01:09 +0530

    shopez-microservices: register product and auth service on Service Registery and implement Config-server

--- authService/src/main/java/com/shopez/authService/AuthServiceApplication.java
+++ authService/src/main/java/com/shopez/authService/AuthServiceApplication.java
@@ -3,9 +3,11 @@ package com.shopez.authService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
