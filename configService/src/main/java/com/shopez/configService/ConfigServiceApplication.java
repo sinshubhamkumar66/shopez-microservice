5df0fdaa39b9cb39396a42849b735bb4d93d1bb2
shubhamsingh <singhshubhamkumar66@gmail.com>
2025-07-22 21:54:20 +0530

    shopez-microservices: register order and payment service on Service Registery and implement Config-server

--- configService/src/main/java/com/shopez/configService/ConfigServiceApplication.java
+++ configService/src/main/java/com/shopez/configService/ConfigServiceApplication.java
@@ -1,15 +1,17 @@
-package com.shopez.PaymentService;
+package com.shopez.configService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentServiceApplication {
+@EnableConfigServer
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
--- orderService/src/main/java/com/shopez/orderService/OrderServiceApplication.java
+++ orderService/src/main/java/com/shopez/orderService/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.shopez.orderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
